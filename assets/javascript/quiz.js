a0360ff4c906a1aaabc0d8089696d25609e1c45c
jalalk1244 <jalaluddin.boss@gmail.com>
2022-09-02 15:34:53 +0000

    Creat function for navigating between sections when clicking on the nav links

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -8,3 +8,33 @@ function openMenu(){
         navListContainer.style.display = "none";
       }
   }
+
+
+// Navigate section to section
+function linkNavigation (event) {
+    let elementId = event.target.id;
+    console.log(elementId);
+    let infoSection = document.getElementById('info-section');
+    let home = document.getElementById('home');
+    let contactSection = document.getElementById('contact-section')
+    if (elementId === 'home-link' || elementId === 'toggle-home-link') {
+        console.log('Home')
+        infoSection.classList.add('hidden');
+        contactSection.classList.add('hidden');
+        home.classList.remove('hidden');
+    } else if (elementId === 'rules-link' || elementId === 'toggle-rules-link') {
+        console.log('Contact or Rules')
+        infoSection.classList.remove('hidden');
+        home.classList.add('hidden');
+        contactSection.classList.add('hidden');
+    } else if (elementId === 'contact-link' || elementId === 'toggle-contact-link') {
+        contactSection.classList.remove('hidden');
+        home.classList.add('hidden');
+        infoSection.classList.add('hidden');
+    }
+}
+
+let links = document.getElementsByClassName('nav-link');
+for (let link of links) {
+    link.addEventListener('click', linkNavigation);
+}
