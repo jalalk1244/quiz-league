cf1cce16206946f4ea393a080ff1327d2a677d9d
jalalk1244 <jalaluddin.boss@gmail.com>
2022-09-07 17:19:49 +0000

    Add 20 more question and add a difficulty property to every question

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -83,77 +83,267 @@ function startGame(e) {
 
 
   // The quiz questions in an array of objects
-let questions = [
+  let questions = [
     {
       "question" : 'Which football club did Lionel Messi join in 2021?',
       "answerA"  : 'FC Barcelona',
       "answerB"  : 'Chelsea',
       "answerC"  : 'Paris Saint-Germain ',
       "answerD"  : 'Manchester United',
-      "correctAnswer" : "c"
+      "correctAnswer" : "c",
+      "difficulty": 'easy'
     },{
       "question" : 'How often is the FIFA World Cup?',
       "answerA"  : 'Every 10 years',
       "answerB"  : 'Every 5 years',
       "answerC"  : 'Every 2 years',
       "answerD"  : 'Every 4 years',
-      "correctAnswer" : "d"
+      "correctAnswer" : "d",
+      "difficulty": 'easy'
     },{
       "question" : 'How long is a standard football game?',
       "answerA"  : '45 min',
       "answerB"  : '120 min',
       "answerC"  : '90 min',
       "answerD"  : '100 min',
-      "correctAnswer" : "c"
+      "correctAnswer" : "c",
+      "difficulty": 'easy'
     },{
       "question" : 'If there is a clean sheet, how many goals were scored?',
       "answerA"  : '0',
       "answerB"  : 'Less than 10',
       "answerC"  : '1',
       "answerD"  : 'Less than 5',
-      "correctAnswer" : "a"
+      "correctAnswer" : "a",
+      "difficulty": 'easy'
     },{
       "question" : 'How many players make a football team?',
       "answerA"  : '7',
       "answerB"  : '11',
       "answerC"  : '22',
       "answerD"  : '9',
-      "correctAnswer" : "b"
+      "correctAnswer" : "b",
+      "difficulty": 'easy'
     },{
       "question" : 'Which of the following is not a football team in England?',
       "answerA"  : 'Chelsea FC',
       "answerB"  : 'Tottenham Hotspur FC',
       "answerC"  : 'Everton FC',
       "answerD"  : 'New England Revolution',
-      "correctAnswer" : "d"
+      "correctAnswer" : "d",
+      "difficulty": 'easy'
     },{
       "question" : 'What are the two primary football teams in Manchester, England?',
       "answerA"  : 'FC Barcelona and Real Madrid',
       "answerB"  : 'Milan and Inter',
       "answerC"  : 'Man United and Man City',
       "answerD"  : 'Bayern Munich and Borussia Dortmund',
-      "correctAnswer" : "c"
+      "correctAnswer" : "c",
+      "difficulty": 'easy'
     },{
       "question" : 'what is the most watched football competition?',
       "answerA"  : 'World Cup',
       "answerB"  : 'Champions League',
       "answerC"  : 'Premier league',
       "answerD"  : 'Europa League',
-      "correctAnswer" : "a"
+      "correctAnswer" : "a",
+      "difficulty": 'easy'
     },{
       "question" : 'Which country is Ronaldo from?',
       "answerA"  : 'Argentina',
       "answerB"  : 'Brazil',
       "answerC"  : 'England',
       "answerD"  : 'Portugal',
-      "correctAnswer" : "d"
+      "correctAnswer" : "d",
+      "difficulty": 'easy'
     },{
       "question" : 'What is the standard size of a football pitch?',
       "answerA"  : '125 m x 85 m',
       "answerB"  : '155 m x 95 m',
       "answerC"  : '175 m x 105 m',
       "answerD"  : '195 m x 110 m',
-      "correctAnswer" : "a"
+      "correctAnswer" : "a",
+      "difficulty": 'easy'
+    },
+    {
+      "question" : 'When was the first Premier League match played?',
+      "answerA"  : '1992/93',
+      "answerB"  : '1991/92',
+      "answerC"  : '1994/95',
+      "answerD"  : '1996/94',
+      "correctAnswer" : "a",
+      "difficulty": 'medium'
+    },
+    {
+      "question" : 'Which player scored the fastest hattrick in the premier League?',
+      "answerA"  : 'Wayen Ronney',
+      "answerB"  : 'Sergio Augero',
+      "answerC"  : 'Sadio Mane',
+      "answerD"  : 'Harry Kane',
+      "correctAnswer" : "c",
+      "difficulty": 'medium'
+    },
+    {
+      "question" : "Which is the world's largest football stadium?",
+      "answerA"  : 'Camp Nou',
+      "answerB"  : 'Wembley Stadium',
+      "answerC"  : 'FNB Stadium',
+      "answerD"  : 'Rungrado 1st of May Stadium',
+      "correctAnswer" : "d",
+      "difficulty": 'medium'
+    },
+    {
+      "question" : 'Which country won the first World Cup Championship?',
+      "answerA"  : 'Brazil',
+      "answerB"  : 'Italy',
+      "answerC"  : 'Uruguay',
+      "answerD"  : 'England',
+      "correctAnswer" : "c",
+      "difficulty": 'medium'
+    },
+    {
+      "question" : 'This Barcelona player was jailed after it was found that his passport was fake',
+      "answerA"  : 'Messi',
+      "answerB"  : 'Ronaldinho',
+      "answerC"  : "Samuel Eto'o",
+      "answerD"  : 'Iniesta',
+      "correctAnswer" : "b",
+      "difficulty": 'medium'
+    },
+    {
+      "question" : 'What was the name of the first football club founded?',
+      "answerA"  : 'Sheffield FC',
+      "answerB"  : 'Stoke City',
+      "answerC"  : 'Queens Park',
+      "answerD"  : 'Wrexham',
+      "correctAnswer" : "a",
+      "difficulty": 'hard'
+    },
+    {
+      "question" : 'Who is the top goal scorer of the Champions League?',
+      "answerA"  : 'Karim Benzema',
+      "answerB"  : 'Lionel Messi',
+      "answerC"  : 'Robert Lewanoski',
+      "answerD"  : 'Cristiano Ronaldo',
+      "correctAnswer" : "d",
+      "difficulty": 'medium'
+    },
+    {
+      "question" : 'Which club has won the most Champions League titles?',
+      "answerA"  : 'Milan',
+      "answerB"  : 'Real Madrid',
+      "answerC"  : 'Barcelona',
+      "answerD"  : 'Man United',
+      "correctAnswer" : "b",
+      "difficulty": 'medium'
+    },
+    {
+      "question" : 'After how many games did Cirstiano Ronaldo score his first Champions League goal?',
+      "answerA"  : '5',
+      "answerB"  : '2',
+      "answerC"  : '15',
+      "answerD"  : '27',
+      "correctAnswer" : "d",
+      "difficulty": 'medium'
+    },
+    {
+      "question" : 'Which football club did Wayne Ronney score his first goal against?',
+      "answerA"  : 'Everton',
+      "answerB"  : 'Arsenal',
+      "answerC"  : 'Tottenham',
+      "answerD"  : 'Man City',
+      "correctAnswer" : "b",
+      "difficulty": 'medium'
+    },
+    {
+      "question" : 'With 365 goals, who holds the record for top Bundesliga goalscorer of all time?',
+      "answerA"  : 'Robert Lewandowski',
+      "answerB"  : 'Klaus Fischer',
+      "answerC"  : 'Gerd Müller',
+      "answerD"  : 'Dieter Müller',
+      "correctAnswer" : "c",
+      "difficulty": 'hard'
+    }, 
+    {
+      "question" : 'After Juventus, AC Milan and Inter, with nine Scudettos, which team has won the most Serie A titles?',
+      "answerA"  : 'Torino',
+      "answerB"  : 'Pro Vercelli',
+      "answerC"  : 'Bologna',
+      "answerD"  : 'Genoa',
+      "correctAnswer" : "d",
+      "difficulty": 'hard'
+    },
+    {
+      "question" : 'Which MLS franchise team does David Beckham own?',
+      "answerA"  : 'Toronto FC',
+      "answerB"  : 'Inter Miami CF',
+      "answerC"  : 'LA Galaxy',
+      "answerD"  : 'FC Dallas',
+      "correctAnswer" : "b",
+      "difficulty": 'hard'
+    },
+    {
+      "question" : "Messi has won a record number of Ballon d'Or awards - how many?",
+      "answerA"  : '4',
+      "answerB"  : '3',
+      "answerC"  : '7',
+      "answerD"  : '5',
+      "correctAnswer" : "c",
+      "difficulty": 'medium'
+    },
+    {
+      "question" : 'Ronaldo is synonymous with the No.7, but what other number did he wear at Real Madrid',
+      "answerA"  : '9',
+      "answerB"  : '27',
+      "answerC"  : '19',
+      "answerD"  : '11',
+      "correctAnswer" : "a",
+      "difficulty": 'hard'
+    },
+    {
+      "question" : "In which World Cup did Diego Maradona score his infamous 'Hand of God' goal?",
+      "answerA"  : 'Spain 1982',
+      "answerB"  : 'Mexico 1986',
+      "answerC"  : 'Italy 1990',
+      "answerD"  : 'United States 1994',
+      "correctAnswer" : "b",
+      "difficulty": 'hard'
+    },
+    {
+      "question" : 'The fastest goal scored in Premier League history came in 7.69 seconds. Who scored it?',
+      "answerA"  : 'Christian Eriksen',
+      "answerB"  : 'Asmir Begovic',
+      "answerC"  : 'Shane Long',
+      "answerD"  : 'Alan Shearer',
+      "correctAnswer" : "c",
+      "difficulty": 'hard'
+    },
+    {
+      "question" : "Who was Jose Mourinho's first signing at Chelsea?",
+      "answerA"  : 'Scott Sinclair',
+      "answerB"  : 'Arjen Robben',
+      "answerC"  : 'Didier Drogba',
+      "answerD"  : 'Paulo Ferreira',
+      "correctAnswer" : "d",
+      "difficulty": 'hard'
+    },
+    {
+      "question" : 'How much did Sporting Lisbon receive for the transfer of 17 years old, Cristiano Ronaldo?',
+      "answerA"  : '£10 million',
+      "answerB"  : '£12 million',
+      "answerC"  : '£5 million',
+      "answerD"  : '£20 million',
+      "correctAnswer" : "b",
+      "difficulty": 'hard'
+    },
+    {
+      "question" : "Which Ballon d'Or-winning footballer had a galaxy named after them in 2015?",
+      "answerA"  : 'Lionel Messi',
+      "answerB"  : 'Kaka',
+      "answerC"  : 'Ronaldinho',
+      "answerD"  : 'Cristiano Ronaldo',
+      "correctAnswer" : "d",
+      "difficulty": 'hard'
     }
   ];
 
